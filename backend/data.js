3f9edad8e5aad3602bc8850e76a8ec368ed63dba
Vladislav <vladrjeousski@gmail.com>
2021-05-18 16:14:35 +0300

    feat: added backend

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: "Shirt 1",
+      category: "Shirts",
+      image: "/images/shirt.png",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      countInStock: 6,
+    },
+    {
+      _id: 2,
+      name: "Shirt 2",
+      category: "Shirts",
+      image: "/images/shirt.png",
+      price: 80,
+      brand: "Nike",
+      rating: 4.5,
+      countInStock: 3,
+    },
+    {
+      _id: 3,
+      name: "Shirt 3",
+      category: "Pants",
+      image: "/images/shirt.png",
+      price: 45,
+      brand: "Nike",
+      rating: 4.5,
+      countInStock: 0,
+    },
+    {
+      _id: 4,
+      name: "Pants",
+      category: "Pants",
+      image: "/images/pants.png",
+      price: 45,
+      brand: "Nike",
+      rating: 4.5,
+      countInStock: 2,
+    },
+  ],
+};
+
+export default data;
