009203131b5a75b3e8e83bdf75357155badb4eb9
Vladislav <vladrjeousski@gmail.com>
2021-05-18 12:47:18 +0300

    feat: added frontend part

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: "Shirt 1",
+      category: "Shirts",
+      image: "/images/shirt.png",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+    },
+    {
+      _id: 2,
+      name: "Shirt 2",
+      category: "Shirts",
+      image: "/images/shirt.png",
+      price: 80,
+      brand: "Nike",
+      rating: 4.5,
+    },
+    {
+      _id: 3,
+      name: "Shirt 3",
+      category: "Pants",
+      image: "/images/shirt.png",
+      price: 45,
+      brand: "Nike",
+      rating: 4.5,
+    },
+    {
+      _id: 4,
+      name: "Pants",
+      category: "Pants",
+      image: "/images/pants.png",
+      price: 45,
+      brand: "Nike",
+      rating: 4.5,
+    },
+  ],
+};
+
+export default data;
